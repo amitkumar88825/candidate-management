b3b26e2348f5c1d2eebbd199ea61376b6df56c40
amitkumar88825 <amitkumaryadav.work@gmail.com>
2024-12-31 13:51:30 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -13,15 +13,19 @@ const app = express();
 app.use(express.json());
 app.use(cors()); 
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 connectDB();
 
 app.get('/', (req, res) => {
   res.send('Welcome to my application!');
 });
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
 app.use('/api', routes());
+
+app.use('*', (req, res) => {
+  res.status(404).send('Route not found');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
