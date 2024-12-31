f24e63064008a86d8606550a74ffabdb99376227
amitkumar88825 <amitkumaryadav.work@gmail.com>
2024-12-31 14:52:41 +0530

    changes

--- backend/routes/candidateRoute.js
+++ backend/routes/candidateRoute.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const verifyToken = require('../middleware/authMiddleware');
 const upload = require('../middleware/upload.js');
+const verifyToken = require('../middleware/authMiddleware');
 
 const candidateController = require('../controllers/candidateController.js')
 
 router.post('/login', candidateController.login);
 
-const verifyToken = require('../middleware/authMiddleware');
 router.get('/:id',verifyToken , candidateController.getCandidateById);
 
 router.post('/profile/:id', verifyToken, upload.single('image'), candidateController.uploadProfileImage);
