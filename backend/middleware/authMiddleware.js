cadf15dfc676559ebf029d9e91d54030f2ab8733
amitkumar88825 <amitkumaryadav.work@gmail.com>
2024-12-31 15:33:36 +0530

    changes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,22 +2,14 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const verifyToken = (req, res, next) => {
-    console.log(5 , 'verify ')
     const token = req.header('Authorization');
 
-    console.log(8 , token)
     if (!token) {
         return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
 
-    console.log(11 , 'dfds')
-
     try {
         const verified = jwt.verify(token, process.env.JWT_SECRET);
-
-        console.log(16 , 'sdfsdf')
-
-        console.log(18 , verified)
 
         req.user = verified;
         next();
